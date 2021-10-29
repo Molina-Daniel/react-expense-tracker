7af59e9e086b9d17dce0cb501ec43dedad090d2f
Daniel <dmolcap@gmail.com>
2021-10-29 21:00:24 +0100

    Add new expense to the expenses array

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,15 @@
-import { useState } from "react"; //imports useState hook
 import ExpenseDate from "./ExpenseData";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  //useState return an array in which the first element is the current state value and the second element is a function to update the value
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
